685bb2be983726c778a3e9370bec5fffbcb61a13
Philipp Stultgens <p.stultgens@gmail.com>
2023-04-14 23:19:04 +0200

    Main Menu implemented with clickable buttons

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,12 +8,7 @@ public class LevelLoader : MonoBehaviour
     [SerializeField] public Animator transition;
     [SerializeField] public float tranistionTime = 1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    
     public void LoadNextLevel()
     {
         StartCoroutine(LoadLevelCoroutine(SceneManager.GetActiveScene().buildIndex + 1));
@@ -22,6 +17,16 @@ public class LevelLoader : MonoBehaviour
     public void LoadLevel(int levelIndex)
     {
         StartCoroutine(LoadLevelCoroutine(levelIndex));
+    }
+
+    public void GoToScene(string sceneName)
+    {
+        StartCoroutine(LoadLevelCoroutine(sceneName));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     IEnumerator LoadLevelCoroutine(int levelIndex)
@@ -34,5 +39,17 @@ public class LevelLoader : MonoBehaviour
 
         // Load Scene
         SceneManager.LoadScene(levelIndex);
+    }
+
+    IEnumerator LoadLevelCoroutine(string sceneName)
+    {
+        // Play animation
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(tranistionTime);
+
+        // Load Scene
+        SceneManager.LoadScene(sceneName);
     }
 }
